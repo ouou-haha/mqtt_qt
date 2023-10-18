69f5627f73f2df8edf457c5d25d2a3f140092ac3
ouou-haha <ou471072445@163.com>
2023-10-18 11:30:30 +0800

    注释代码

--- untitled/healthmsg.cpp
+++ untitled/healthmsg.cpp
@@ -1,5 +1,15 @@
 #include "healthmsg.h"
 
+
+
+/***************************************************************
+  *  函数名称：healthmsg::healthmsg(构造函数)
+  *  函数作用：初始化
+  *  输入参数：ip port（端口） topic（主题）  button（订阅主题按钮）  label_mbp label_mhr (显示两个数据的位置)
+  *  返 回 值：无
+  *  函数的使用方法：在主函数中创建类名并直接调用函数
+  *  日    期： 2023.10.18
+ **************************************************************/
 healthmsg::healthmsg(QString hostname, QString port, QString topic, QPushButton *button, QLabel *label_mbp, QLabel *label_mhr)
 {
     client = new QMqttClient(this);
@@ -11,20 +21,32 @@ healthmsg::healthmsg(QString hostname, QString port, QString topic, QPushButton 
     this->label_mhr = label_mhr;
 }
 
+
+// 析构函数
 healthmsg::~healthmsg(){
     delete client;
 }
 
-
+/***************************************************************
+  *  函数名称： healthmsg::connect()
+  *  函数作用：连接到mqtt代理服务器
+  *  输入参数：无
+  *  返 回 值：无
+  *  函数的使用方法：在主函数中创建类名并直接调用函数
+  *  日    期： 2023.10.18
+ **************************************************************/
 void healthmsg::connect(){
+
+    // 对client进行配置
     this->client->setHostname(this->hostname);
     this->client->setPort(this->port.toInt());
     this->client->connectToHost();
     this->client->subscribe(topic);
 
-
+    // 判断当前连接的状态（槽函数）连接不上打印1 连接成功打印2
     QAbstractSocket::connect(this->client, &QMqttClient::stateChanged, this, &healthmsg::updateLogStateChange);
 
+    // 监听消息接收显示（槽函数）
     QAbstractSocket::connect(client, &QMqttClient::messageReceived, this, [this](const QByteArray &message, const QMqttTopicName &topic) {
         this->msg = QDateTime::currentDateTime().toString()
                                 + QLatin1String(" Received Topic: ")
@@ -32,12 +54,15 @@ void healthmsg::connect(){
                                 + QLatin1String(" Message: ")
                                 + message
                                 + QLatin1Char('\n');
-
+        // 打印消息
         qDebug()<< this->msg;
         this->data = message;
+
+        // 解析数据
         analysisMsg();
         label_mbp->setText(getMBP());
         label_mhr->setText(getMHR());
+        // 获取数据
         qDebug()<< getMBP();
 
     });
@@ -50,6 +75,7 @@ void healthmsg::connect(){
         qDebug()<< this->msg;
     });
 
+    // 订阅主题的按键
     QAbstractButton::connect(sub_button, &QPushButton::clicked, [this]{
         client->subscribe(this->topic);
     });
@@ -67,6 +93,8 @@ void healthmsg::updateLogStateChange(){
     qDebug()<< content;
 }
 
+
+
 void healthmsg::on_buttonSubscribe_clicked()
 {
     auto subscription = client->subscribe(topic);
@@ -76,6 +104,16 @@ void healthmsg::on_buttonSubscribe_clicked()
     }
 }
 
+
+
+/***************************************************************
+  *  函数名称： healthmsg::analysisMsg()
+  *  函数作用： 数据解析函数 将string转成json再获取对应键值
+  *  输入参数： 无
+  *  返 回 值：无
+  *  函数的使用方法：内部调用
+  *  日    期： 2023.10.18
+ **************************************************************/
 void  healthmsg::analysisMsg(){
     jsonData = QJsonDocument::fromJson(data.toUtf8(), &jsonError);
     if (!jsonData.isNull()){
--- untitled/healthmsg.h
+++ untitled/healthmsg.h
@@ -18,7 +18,7 @@ public:
     healthmsg(QString hostname, QString port, QString topic, QPushButton *button, QLabel *label_mbp, QLabel *label_mhr);
     ~healthmsg();
 
-
+    // 连接代理服务器
     void connect();
 
 public slots:
@@ -26,24 +26,44 @@ public slots:
     void on_buttonSubscribe_clicked();
 
 private:
+    // 需要监听的ip地址
     QString hostname;
+
+    // 需要监听的端口
     QString port;
+
+    // 需要监听的主题（了解mqtt的主题概念）
     QString topic;
+
+    // 接收到的信息
     QString msg;
+
+    // 订阅主题按键
     QPushButton *sub_button;
+
+    // 显示数据的label 血氧 血压
     QLabel *label_mbp;
     QLabel *label_mhr;
+
+    // 数据解析
     QString data;
     QJsonParseError jsonError;
     QJsonDocument jsonData;
 
+    // 血氧 血压数据
     QString mHR;
     QString mBP;
 
+
+    // mqtt对象
     QMqttClient *client;
 
+  //  *****************内部调用函数****************************
+
+    // 数据解析函数 解析msg
     void analysisMsg();
 
+    // 获取血氧 血压数据
     QString getMHR();
     QString getMBP();
 };
